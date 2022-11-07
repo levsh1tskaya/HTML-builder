eb6315aa8b34dd5ffd1afe8cc057d7694ff6ed1d
Anastasia Levshitskaya <anastasialevsh@mail.ru>
2022-11-08 02:24:23 +0300

    feat: add greeting message and input

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,26 @@
-const process = require('process');
+const fs = require ('fs')
 const readline = require('readline');
-const emitter = require('events');
-const stream = require('stream');
 const path = require('path')
 
-let writeableStream = fs.createWriteStream("some.txt")
+const writeStream = fs.createWriteStream (
+  path.resolve(__dirname, './text.txt')
+);
+
+const rlInter = readline.createInterface({ 
+  input: process.stdin,
+  output: process.stdout
+ });
+var message = 'Приветствую!'
+console.log(message)
+ rlInter.setPrompt('Введите строку или нажмите "exit", чтобы выйти: ');
+
+rlInter.on('line', (line) => {
+  if (line === 'exit') {
+    rlInter.close();
+    return
+  }
+
+  writeStream.write(`${line}\n`);
+
+});
+rlInter.on('SIGINT', rlInter.close)
