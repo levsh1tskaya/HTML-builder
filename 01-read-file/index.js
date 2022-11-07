348a3d053bf00b822fc1a97e188f857d9d00c384
Anastasia Levshitskaya <anastasialevsh@mail.ru>
2022-11-07 03:18:33 +0300

    feat: add read file script

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,2 +1,9 @@
 const fs = require ('fs')
 const path = require('path')
+const stream = require('stream');
+
+var directory = path.join(__dirname, 'text.txt')
+var readStream = fs.createReadStream(directory)
+readStream.on ('data', (chunk) => {
+   console.log(chunk.toString());
+})
